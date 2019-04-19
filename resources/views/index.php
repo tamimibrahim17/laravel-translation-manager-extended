e4713e5369d79093a3f6c1e7375b82f69b8f8478
Shuvro Roy <shuvro.nsu.cse@gmail.com>
2019-04-19 13:46:53 +0100

    WIP

--- resources/views/index.php
+++ resources/views/index.php
@@ -319,7 +319,11 @@
                         <tr>
                             <th width="15%">Key</th>
                             <?php foreach ($locales as $locale): ?>
+                            <?php if($locale == $keylocale): ?>
                                 <th><?= $locale ?></th>
+                            <?php else: continue ?>
+                            <?php endif; ?>
+                                
                             <?php endforeach; ?>
                             <?php if ($deleteEnabled): ?>
                                 <th>&nbsp;</th>
@@ -329,36 +333,19 @@
                         <tbody>
 
                         <?php foreach ($emTranslations as $key => $emTranslation): ?>
+
                             <tr id="<?php echo htmlentities($key, ENT_QUOTES, 'UTF-8', false) ?>">
                                 <td><?php echo htmlentities($key, ENT_QUOTES, 'UTF-8', false) ?></td>
-                                <?php foreach ($locales as $locale): ?>
-                                    <?php $t = isset($emTranslation[$locale]) ? $emTranslation[$locale] : null ?>
-
+                                <?php $t = null ?>
                                     <td>
                                         <div style="display: flex;">
                                             <a href="#edit" style="margin-right: 6px;" class="editable status-<?php echo $t ? $t->status : 0 ?> locale-<?php echo $locale ?>"
-                                        data-locale="<?php echo $locale ?>" data-name="<?php echo $locale . "|" . htmlentities($key, ENT_QUOTES, 'UTF-8', false) ?>"
+                                        data-locale="<?php echo $keylocale ?>" data-name="<?php echo $keylocale . "|" . htmlentities($key, ENT_QUOTES, 'UTF-8', false) ?>"
                                         id="username" data-type="textarea" data-pk="<?php echo $t ? $t->id : 0 ?>"
                                         data-url="<?php echo $editUrl ?>"
                                         data-title="Enter translation"><?php echo $t ? htmlentities($t->value, ENT_QUOTES, 'UTF-8', false) : '' ?></a>
-
-                                        <?php if ($t && $t->status == 1): ?>
-                                        <form action="<?php echo action('\ShuvroRoy\TranslationManager\Controller@postApprove', [$group]) ?>" method="POST" role="form">
-                                                    <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
-                                                    <input type="hidden" name="local" value="<?php echo $locale; ?>">
-                                                    <input type="hidden" name="key" value="<?php echo htmlentities($key, ENT_QUOTES, 'UTF-8', false) ?>">
-                                                    <input type="hidden" name="previous" value="<?php echo url()->full(); ?>">
-                                                    <input type="hidden" name="content" value="<?php echo $t->value; ?>">
-                                                    <input type="hidden" name="id" value="<?php echo $t->id; ?>">
-                                                    <span class="form-group">
-                                                        <button type="submit" class="btn btn-xs btn-success"><span
-                                                        class="glyphicon glyphicon-ok"></span></button>
-                                                    </span>
-                                                </form>
-                                            <?php endif; ?>
                                         </div>
                                     </td>
-                                <?php endforeach; ?>
                                 <?php if ($deleteEnabled): ?>
                                     <td>
                                         <a href="<?php echo action('\ShuvroRoy\TranslationManager\Controller@postDelete', [$group, $key]) ?>"

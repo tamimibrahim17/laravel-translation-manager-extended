8d269556942ab78fd8e66b37cf7110fb39bc6bcd
Shuvro Roy <shuvro.nsu.cse@gmail.com>
2019-05-02 20:00:37 +0600

    WIP

--- resources/views/index.php
+++ resources/views/index.php
@@ -283,12 +283,12 @@
                         </tr>
                         </thead>
                         <tbody>
-
-                        <?php $i = 1; foreach ($translations as $key => $translation): ?>
+                        <?php $i = 1; ?>
+                        <?php foreach ($translations as $key => $translation): ?>
                             <tr id="<?php echo htmlentities($key, ENT_QUOTES, 'UTF-8', false) ?>">
                                 <td>
                                     <?php echo htmlentities($key, ENT_QUOTES, 'UTF-8', false) ?>
-                                    <span><button style="margin-right:3px;" type="button" class="btn btn-xs btn-info" data-toggle="modal" data-target="#myModalPath<?php echo $i; ?>"><span class="glyphicon glyphicon-asterisk"></span></button></span>
+                                    <span><button style="margin-right:3px;" type="button" class="btn btn-xs btn-warning" data-toggle="modal" data-target="#myModalPath<?php echo $i; ?>"><span class="glyphicon glyphicon-road"></span></button></span> <span style="margin-left: 2px;"><?php echo \ShuvroRoy\TranslationManager\Models\Translation::where('locale', config('app.locale'))->where('key', $key)->count(); ?></span>
                                     <!-- Modal -->
                                     <div class="modal fade" id="myModalPath<?php echo $i; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
                                         <div class="modal-dialog" role="document">
@@ -315,6 +315,7 @@
                                             </div>
                                         </div>
                                     </div>
+                                    <?php $i++; ?>
                                 </td>
                                 <?php foreach ($locales as $locale): ?>
                                     <?php $t = isset($translation[$locale]) ? $translation[$locale] : null ?>
